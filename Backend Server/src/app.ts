9bf8a211710edbad048aeac844d6a80eaedc6529
Tomás Santos <75134545+tomassantos99@users.noreply.github.com>
2022-01-27 00:21:18 +0000

    Attempt at fixing cors problems.

--- Backend Server/src/app.ts
+++ Backend Server/src/app.ts
@@ -17,6 +17,12 @@ export class App {
     this.app.set("port", 8080);
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(function (req, res, next) {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+      res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+      next();
+    });
     this.app.use("/users", [userRoutes, associationRoutes]);
     this.app.use("/donation", donationRoutes);
     this.app.disable("etag");
